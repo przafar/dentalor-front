27c8b896bf975f6d73487ed70868ca12771d4ddd
Zafar <su10409@studenti.polito.it>
2025-02-18 22:13:44 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 3000,
     proxy: {
       '/api/': {
         target: 'https://med.mf.uz/api/web/',
